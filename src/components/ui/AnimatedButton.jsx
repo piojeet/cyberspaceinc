e051a784d931e7d096d914ad81b191bc3b1f16f6
piojeet <manjhipiojeet@gmail.com>
2025-10-23 13:17:50 +0530

    first commit

--- src/components/ui/AnimatedButton.jsx
+++ src/components/ui/AnimatedButton.jsx
@@ -12,31 +12,23 @@ export default function AnimatedButton({
     <>
       <span className="span-mother">
         {label.split("").map((char, i) => (
-          <span key={i}>{char}</span>
+          <span key={i}>{char === " " ? "\u00A0" : char}</span>
         ))}
       </span>
       <span className="span-mother2">
         {label.split("").map((char, i) => (
-          <span key={i}>{char}</span>
+          <span key={i}>{char === " " ? "\u00A0" : char}</span>
         ))}
       </span>
     </>
   );
 
   return to ? (
-    <NavLink
-      to={to}
-      className={`animated-ui ${className}`}
-      style={style}
-    >
+    <NavLink to={to} className={`animated-ui ${className}`} style={style}>
       <InnerContent />
     </NavLink>
   ) : (
-    <button
-      onClick={onClick}
-      className={`animated-ui ${className}`}
-      style={style}
-    >
+    <button onClick={onClick} className={`animated-ui ${className}`} style={style}>
       <InnerContent />
     </button>
   );
